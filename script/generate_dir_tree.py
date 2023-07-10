02f756db292b517235b55251294ffc1dd3879f3b
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2023-07-10 12:42:36 +0200

    Use datatables to display models (#2)
    * use index instead of dict name
    
    * remove style, initialize data table
    
    * add remote resources, restructure assets
    
    * add intro, top folder `tr`, style nowrap
    
    * enable horizontal scroll, add empty cell
    
    * print extension, update allowed extensions
    
    * widen view for large display
    
    * enable row grouping
    
    * add rowgroup script, sort ascending
    
    * make rowGroup script local to page
    
    * change default row num to 25
    
    * add clarification about grouping
    
    * link logo to project page, clean up config
    
    * remove redundant repo link
    
    * open project page in new tab

--- script/generate_dir_tree.py
+++ script/generate_dir_tree.py
@@ -1,43 +1,56 @@
 import glob
 import os
 import yaml
+from pprint import pprint
 
 
 ROOT_DIR = os.path.join(os.path.dirname(__file__), "..")
-SKIP_TOP_DIRS = ["images", "_data", "script"]
-EXTENSIONS = [".json", ".provn", ".rosparams", ".yaml"]
+SKIP_TOP_DIRS = ["assets", "_data", "script"]
+IGNORE_EXTENSIONS = [".html", ".md", ""]
 OUTPUT_FILE = "file_paths.yml"
 OUTPUT_DIR = os.path.join(ROOT_DIR, "_data")
 
 
 def main():
+    extensions = set()
     file_list = glob.glob(os.path.join(ROOT_DIR, "**"), recursive=True)
     filtered_file_data = {}
     for file_path in file_list:
+        if os.path.isdir(file_path):
+            continue
         rel_path = os.path.relpath(file_path, ROOT_DIR)
+        skip_dir = False
         for skip_top_dir in SKIP_TOP_DIRS:
             if rel_path.startswith(skip_top_dir):
-                continue
+                skip_dir = True
+                break
+        if skip_dir:
+            continue
+
         dir_name, file_name = os.path.split(rel_path)
         if not dir_name:
             # skip top directory
             continue
         file_base_name, extension = os.path.splitext(file_name)
-        if extension not in EXTENSIONS:
+        extensions.add(extension)
+        if extension in IGNORE_EXTENSIONS:
             continue
 
         top_dir = dir_name.split(os.sep)[0]
         if top_dir not in filtered_file_data:
             filtered_file_data[top_dir] = []
-        filtered_file_data[top_dir].append(rel_path)
+        filtered_file_data[top_dir].append(
+            {"path": rel_path, "name": file_name, "extension": extension[1:].upper()}
+        )
+    print("detected the following extensions:")
+    pprint(extensions)
 
     if not os.path.exists(OUTPUT_DIR):
         os.makedirs(OUTPUT_DIR)
 
-    with open(os.path.join(OUTPUT_DIR, OUTPUT_FILE), 'w') as out_file:
+    with open(os.path.join(OUTPUT_DIR, OUTPUT_FILE), "w") as out_file:
         yaml.dump(filtered_file_data, out_file)
 
 
 if __name__ == "__main__":
     main()
-
